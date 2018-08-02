4088b9d812696548f3bd2e275d78b22163575866
pprogs <kchurilov@navicons.ru>
2018-08-02 19:05:52 +0300

    dist

---
+++ dist/precache-manifest.785f75f149fd30bfe23aa2bfba399819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "041a2d231bae72ebdc66",
+    "url": "/js/chunk-vendors.041a2d23.js"
+  },
+  {
+    "revision": "8a991b6c1fe22fd7a3b9",
+    "url": "/js/app.8a991b6c.js"
+  },
+  {
+    "revision": "adbf3f28bee3cb6ede85",
+    "url": "/js/about.adbf3f28.js"
+  },
+  {
+    "revision": "e2b4f736d894c0ee1349e0533aef369d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8a991b6c1fe22fd7a3b9",
+    "url": "/css/app.dd7f6e46.css"
+  }
+];
